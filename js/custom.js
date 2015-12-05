83257a93674d82ffc438292242b968948fe896e9
AdamLuptak <adamluptakosice@gmail.com>
2015-12-05 23:05:52 +0100

    wewewewwev slidshow treba logo

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	/* wow ======================================= */
+
+	new WOW().init({
+		offset: 20 
+	});
+
+
+	/* Hero slider ======================================= */
+
+	$('#hero-slides').superslides({
+		play: 4000,
+		animation: 'fade'
+	});
+
+
+});
