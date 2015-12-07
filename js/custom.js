3a98d0953becc953dd1673c92b613e439528c39a
AdamLuptak <adamluptakosice@gmail.com>
2015-12-07 23:04:47 +0100

    de slider

--- js/custom.js
+++ js/custom.js
@@ -18,8 +18,43 @@ $(document).ready(function() {
 					theme:"buttons-in"
 				});
 
+
+$(function(){
+  $('.crsl-items').carousel({
+    visible: 3,
+    itemMinWidth: 180,
+    itemEqualHeight: 370,
+    itemMargin: 9,
+  });
+  
+  $("a[href=#]").on('click', function(e) {
+  	console.log("teraz");
+  
+   
+   e.preventDefault();
+
+
+ 
+
+
+  });
+
+  
+});
+
+// $('.next,.previous').on('click',function(e){
+//    $('.next,.previous').attr("disabled", "disabled");
+// setTimeout('enableButton()', 500);
+
+// });
+
+
  
 });function co(){
 
 	console.log("idem to");
+}
+
+  function enableButton(){
+   $('a[href=#]').removeAttr('disabled');
 }
---
+++ scripts/controller/productCatalogController.js
@@ -0,0 +1,47 @@
+app.controller('ProductCatalogController',['$scope',function($scope){
+
+	$scope.products=[
+	{
+		"id": 1,
+		"url": "thumbnails/01.jpg",
+		"price" : 500,
+		"name" : "case 1"
+	},
+	{
+		"id": 2,
+		"url": "thumbnails/02.jpg",
+		"price" : 500,
+		"name" : "case 1"
+	},
+	{
+		"id": 3,
+		"url": "thumbnails/03.jpg",
+		"price" : 500,
+		"name" : "case 1"
+	},
+	{
+		"id": 4,
+		"url": "thumbnails/04.jpg",
+		"price" : 500,
+		"name" : "case 1"
+	},
+	{
+		"id": 5,
+		"url": "thumbnails/05.jpg",
+		"price" : 500,
+		"name" : "case 1"
+	},
+	{
+		"id": 6,
+		"url": "thumbnails/06.jpg",
+		"price" : 500,
+		"name" : "case 1"
+	}
+
+	];
+
+
+
+
+
+}]);
